133bed109b43fe77eaa9e6c8484157ef1bfa621b
dupuyjs <js.dupuy@live.com>
2016-06-05 18:37:22 +0200

    Initial commit

--- EventsLookup/EventsLookup/Helpers/Keys.cs
+++ EventsLookup/EventsLookup/Helpers/Keys.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EventsLookup.Helpers
+﻿namespace EventsLookup.Helpers
 {
     public static class Keys
     {
-        public const string MeetupApiKey = @"5913182f503a73342030315a255b5b40";
+        public const string MeetupApiKey = @"";
     }
 }
